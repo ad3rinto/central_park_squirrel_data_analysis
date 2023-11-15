f73a1c54200f6f5fb6ce0c08b32d6be23373198c
ad3rinto <aad3rinto@gmail.com>
2023-11-15 17:56:47 +0000

    read csv data

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import csv
 import pandas as pd
 
+with open("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20231115.csv", mode="r") as csvfile:
+    data = pd.read_csv(csvfile)
+    print(data["Primary Fur Color"].describe())
