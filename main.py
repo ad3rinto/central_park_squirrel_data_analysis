3361d68d85bedf1ae441efb74eae78279b6ed9f3
ad3rinto <aad3rinto@gmail.com>
2023-11-16 10:12:54 +0000

    NFI

--- main.py
+++ main.py
@@ -3,4 +3,16 @@ import pandas as pd
 
 with open("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20231115.csv", mode="r") as csvfile:
     data = pd.read_csv(csvfile)
-    print(data["Primary Fur Color"].describe())
+    gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+    red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+    black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+    # print(gray_squirrels_count, red_squirrels_count, black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pd.DataFrame(data_dict)
+print(df)
+df.to_csv("squirrel_count.csv")
